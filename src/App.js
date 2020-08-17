74b7dca5143228f7d14d651d3f453b922a4e533d
Dmitry Mordvintsev <d.mordvincev@systtech.ru>
2020-08-17 23:18:19 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Slider from './components/Slider';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <Slider />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Slider/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+class Slider extends Component {
+  render() {
+    const settings = {
+      spaceBetween: 50,
+      slidesPerView: 1,
+      navigation: true,
+      pagination: {clickable: true },
+      scrollbar: { draggable: true },
+    }
+    return (
+      <Swiper {...settings}>
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    );
+  }
+}
+
+export default Slider;
